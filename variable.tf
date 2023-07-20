bda1db4fadc2e73f98b2f68389a176dc24ecd650
gaurinpatole <gauri.patole@appliedaiconsulting.com>
2023-07-20 16:04:44 +0530

    cleaned variable file.

--- variable.tf
+++ variable.tf
@@ -1,55 +1,19 @@
-variable "project" {
-  description = "Project or organisation name"
-  type        = string
-  default     = ""
-}
-
-variable "environment" {
-  description = "Application environment name (dev/prod/qa)"
-  type        = string
-  default     = ""
-}
-
-variable "username" {
-  description = "Name of the user"
-  type        = string
-  default     = ""
-}
-
 variable "aws_region_name" {
   description = "AWS region where all the resources are going to create"
   type        = string
   default     = "us-east-1"
 }
 
-variable "versioning" {
-  type        = string
-  description = "Either you want to enable versioning for S3 bucket or not"
-  default     = ""
-}
-
-variable "acl" {
-  type        = string
-  description = "It defines which AWS accounts or groups are granted access and the type of access"
-  default     = ""
-}
-
 variable "domain_name" {
   type        = string
   description = "The domain name for the website"
-  default     = ""
+  default     = "gauri.dev.devopsinabox.aaic.cc"
 }
 
 variable "bucket_name" {
   type        = string
   description = "The name of the bucket without the www. prefix(normally domain_name)"
   default     = "gauritest1"
-}
-
-variable "validation_method" {
-  type        = string
-  description = "Give validation method to validate the ssl certificate(DNS or EMAIL)"
-  default     = ""
 }
 
 variable "aws_route53_record" {
@@ -69,20 +33,8 @@ variable "website_additional_domains" {
   default     = "gauri.dev.devopsinabox.aaic.cc"
 }
 
-variable "bucket_versioning" {
-  description = "Enable S3 bucket versioning (true or false)."
-  type        = bool
-  default     = false
-}
-
 variable "validate_certificate" {
   description = "Whether to validate certificate by creating Route53 record"
   type        = bool
   default     = true
-}
-
-variable "sub_domain_name" {
-  type        = string
-  description = "The Additional (sub) domain name for the website"
-  default     = "gauri.dev.devopsinabox.aaic.cc"
 }
