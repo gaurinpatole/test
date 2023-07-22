5df151a3f1daa0036d83072a1c3e4b0af6c1c292
gaurinpatole <138434761+gaurinpatole@users.noreply.github.com>
2023-07-23 01:01:35 +0530

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -20,11 +20,6 @@ origin {
     }
   }
 
-output "cloudfront_distribution_domain_name" {
-  description = "The domain name of the CloudFront distribution."
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
-
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
@@ -41,6 +36,11 @@ output "cloudfront_distribution_domain_name" {
       }
     }
   }
+
+output "cloudfront_distribution_domain_name" {
+  description = "The domain name of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
+}
 
   restrictions {
     geo_restriction {
