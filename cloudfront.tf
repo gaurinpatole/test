4f529ea03a854b071997147aa1c9b23fd699c420
gaurinpatole <138434761+gaurinpatole@users.noreply.github.com>
2023-07-23 11:52:54 +0530

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -59,8 +59,3 @@ origin {
     response_page_path    = "/error.html"
   }
 }
-
-output "cloudfront_distribution_domain_name" {
-  description = "The domain name of the CloudFront distribution."
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
